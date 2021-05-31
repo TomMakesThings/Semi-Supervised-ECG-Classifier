c9af1464d655dd933836420b2ec9ff0135788c8f
TomMakesThings <61354833+TomMakesThings@users.noreply.github.com>
2021-05-31 13:35:03 +0100

    Added index page

---
+++ Javascript/navbar.js
@@ -0,0 +1,28 @@
+function profileHover(element) {
+  element.setAttribute('src', "Assets/Images/TomMakesThings-Blink.gif");
+}
+
+function profileUnhover(element) {
+  element.setAttribute('src', "Assets/Images/TomMakesThings.png");
+}
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  all_content = document.getElementsByClassName('header');
+  var position = "absolute";
+  var top = "0px";
+  if (window.innerWidth < 720) {
+    if (document.body.scrollTop > 140 || document.documentElement.scrollTop > 140) {
+      position = "fixed";
+      top = "-140px";
+    }
+  }
+  else {
+    position = "fixed";
+  }
+  for (i = 0; i < all_content.length; i++) {
+    all_content[i].style.position = position;
+    all_content[i].style.top = top;
+  }
+}
